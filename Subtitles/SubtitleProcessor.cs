c833b9e18732f7f06b423de78083e3b712efc25b
Jason Dove <jason@jasondove.me>
2022-10-20 08:33:39 -0500

    fix empty subtitle bug (#25)
    * fix bug with empty extracted subtitle
    
    * update dependencies

--- Subtitles/SubtitleProcessor.cs
+++ Subtitles/SubtitleProcessor.cs
@@ -18,32 +18,39 @@ public class SubtitleProcessor
 
     public async Task<Either<Exception, List<string>>> ConvertToLines(ExtractedSubtitles subtitles)
     {
-        Either<Exception, ExtractedSrtSubtitles> subRip = subtitles switch
+        try
         {
-            ExtractedSrtSubtitles srt => srt,
-            ExtractedDvdSubtitles dvd => await Ocr(dvd),
-            ExtractedPgsSubtitles pgs => await Ocr(pgs),
-            _ => throw new IndexOutOfRangeException(nameof(subtitles))
-        };
+            Either<Exception, ExtractedSrtSubtitles> subRip = subtitles switch
+            {
+                ExtractedSrtSubtitles srt => srt,
+                ExtractedDvdSubtitles dvd => await Ocr(dvd),
+                ExtractedPgsSubtitles pgs => await Ocr(pgs),
+                _ => throw new IndexOutOfRangeException(nameof(subtitles))
+            };
 
-        foreach (ExtractedSrtSubtitles srt in subRip.RightToSeq())
-        {
-            var parser = new SrtParser();
-            await using FileStream fs = File.OpenRead(srt.FileName);
-            Option<List<SubtitleItem>> parsed = parser.ParseStream(fs, Encoding.UTF8);
-            return parsed.Map(list => list.SelectMany(l => l.PlaintextLines))
-                .Flatten()
-                .Map(line => line.ToLowerInvariant())
-                .Map(line => line.Replace("||", "ll"))
-                .Map(line => BadApostrophe.Replace(line, "'"))
-                .Map(line => NamesAndDashes.Replace(line, string.Empty))
-                .Map(line => line.Trim())
-                .Filter(line => !string.IsNullOrWhiteSpace(line))
-                .Filter(line => !Sdh.Match(line).Success)
-                .ToList();
+            foreach (ExtractedSrtSubtitles srt in subRip.RightToSeq())
+            {
+                var parser = new SrtParser();
+                await using FileStream fs = File.OpenRead(srt.FileName);
+                Option<List<SubtitleItem>> parsed = parser.ParseStream(fs, Encoding.UTF8);
+                return parsed.Map(list => list.SelectMany(l => l.PlaintextLines))
+                    .Flatten()
+                    .Map(line => line.ToLowerInvariant())
+                    .Map(line => line.Replace("||", "ll"))
+                    .Map(line => BadApostrophe.Replace(line, "'"))
+                    .Map(line => NamesAndDashes.Replace(line, string.Empty))
+                    .Map(line => line.Trim())
+                    .Filter(line => !string.IsNullOrWhiteSpace(line))
+                    .Filter(line => !Sdh.Match(line).Success)
+                    .ToList();
+            }
+
+            return new Exception("Failed to convert lines");
         }
-
-        return new Exception("Failed to convert lines");
+        catch (Exception ex)
+        {
+            return ex;
+        }
     }
 
     private async Task<Either<Exception, ExtractedSrtSubtitles>> Ocr(ExtractedDvdSubtitles dvd)
